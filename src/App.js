0a1a520e087542559c772a6197059360566bb30d
luis <luisnavarroFS@gmail.com>
2024-02-12 11:00:43 +0100

    add RouterProvider & appRouter

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from  'react-router-dom'
+import HomePage from './pages/home';
+import SearchPage from './pages/search';
+import PetDetailsPage from './pages/detail';
+import PetDetailsNotFound from './pages/petDetailsNotFound';
+import Root from './components/root';
+
+// Add react-router-dom imports
+
+// create router with JSX Route elements
+const appRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<Root/>}>
+    <Route path='/' element={<HomePage />} />
+    <Route path='/:type' element={<HomePage />} />
+    <Route path='/:type/:id' element={<PetDetailsPage />} />
+  </Route>
+));
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={appRouter}/>
+    </>
+  );
+}
+
+export default App;
