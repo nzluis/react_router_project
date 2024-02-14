598fc37e6a6312e3de7315aabf1292213c406c41
luis <luisnavarroFS@gmail.com>
2024-02-14 21:33:50 +0100

    refactor: clean comments

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -31,7 +31,7 @@ const HomePage = () => {
       {data.length ? (
         <div className="grid">
           {data.map((animal) => (
-            <Link // Change me to a Link!
+            <Link 
               key={animal.id}
               to={`/${animal.type.toLowerCase()}/${animal.id}`}
               className="pet"
